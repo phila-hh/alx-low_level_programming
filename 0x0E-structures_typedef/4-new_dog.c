f8ca9113a75550a67056419fc3bdb45a344c5056
phila-hh <filimon.haftomh@gmail.com>
2023-04-17 08:09:23 -0700

    Hotfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,9 +25,69 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = name;
+	new->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new->owner == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
+
+	new->name = _strcpy(new->name, name);
 	new->age = age;
-	new->owner = owner;
+	new->owner = _strcpy(new->owner, owner);
 
 	return (new);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character pointer
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+	while (s[i])
+	{
+		len++;
+		i++;
+	}
+
+	return (len);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src, including the terminating
+ * null byte (\0), to the buffer pointed to by dest
+ * @dest: character pointer to dest
+ * @src: character pointer to src
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,8 @@ struct dog
 typedef struct dog dog_t;
 
 char _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
