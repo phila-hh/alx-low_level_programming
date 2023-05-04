00a2dbed5d5f12b6d89ba04d67c13a0501ad9f30
phila-hh <filimon.haftomh@gmail.com>
2023-05-04 09:47:09 -0700

    Hotfix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,30 +9,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
-	int len, mul;
+	unsigned int num, mul;
+	int len;
 
-	if (b == NULL)
+	if (b == '\0')
 	{
 		return (0);
 	}
 
-	for (len = 0; b[len]; len++)
+	for (len = 0; b[len];)
+	{
+		len++;
+	}
+
+	num = 0;
+	mul = 1;
+	for (len -= 1; len >= 0; len--)
 	{
 		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
-	}
-
-	len--;
-	num = 0;
-	mul = 1;
-	while (len >= 0)
-	{
 		num += (b[len] - '0') * mul;
 		mul *= 2;
-		len--;
 	}
 
 	return (num);
